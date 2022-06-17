b79965c3305dd84c477a471695f0e7877d1119b5
Olly Holovchenko <oleh.holo@gmail.com>
2022-06-17 18:10:58 +0100

    Add screen component with stories and mock requests.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -79,7 +79,7 @@ TaskList.propTypes = {
   /** Checks if it's in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
-  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  tasks: PropTypes.arrayOf(Task.propTypes.task),
   /** Event to change the task to pinned */
   onPinTask: PropTypes.func,
   /** Event to change the task to archived */
