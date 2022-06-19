9b11bd92983ce506fdb69169546fc61f6cf38378
Olly Holovchenko <oleh.holo@gmail.com>
2022-06-19 21:37:15 +0100

    Remove blue bg.

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,7 +19,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
         />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" style={{ background: 'blue' }} />
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
